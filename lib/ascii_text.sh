dd82dd420381a4a82aa5dc72f11e1688ac3fafe8
sourav <souravmallik926@gmail.com>
2026-02-21 21:31:28 +0530

    style: change ASCII font to 4-line line-art and add ✦ separator

--- lib/ascii_text.sh
+++ lib/ascii_text.sh
@@ -1,61 +1,62 @@
 #!/usr/bin/env bash
 # ── AuraFetch: ASCII Text Generator ────────────────────────
-# Simple 3-high block font for zero-dependency ASCII art
+# 4-line line-art font for zero-dependency ASCII art
 
 render_ascii_text() {
     local input=$(echo "$1" | tr '[:lower:]' '[:upper:]')
     local -n _output=$2
-    local line1="" line2="" line3=""
+    local line1="" line2="" line3="" line4=""
 
     for (( i=0; i<${#input}; i++ )); do
         local char="${input:$i:1}"
         case "$char" in
-            A) line1+=" █▀█ "; line2+=" █▀█ "; line3+=" ▀ ▀ " ;;
-            B) line1+=" █▀▄ "; line2+=" █▀▄ "; line3+=" ▀▀  " ;;
-            C) line1+=" █▀▀ "; line2+=" █ ▄ "; line3+=" ▀▀▀ " ;;
-            D) line1+=" █▀▄ "; line2+=" █ █ "; line3+=" ▀▀  " ;;
-            E) line1+=" █▀▀ "; line2+=" █▀▀ "; line3+=" ▀▀▀ " ;;
-            F) line1+=" █▀▀ "; line2+=" █▀▀ "; line3+=" ▀   " ;;
-            G) line1+=" █▀▀ "; line2+=" █ █ "; line3+=" ▀▀▀ " ;;
-            H) line1+=" █ █ "; line2+=" █▀█ "; line3+=" ▀ ▀ " ;;
-            I) line1+="  █  "; line2+="  █  "; line3+="  ▀  " ;;
-            J) line1+="   █ "; line2+=" █ █ "; line3+=" ▀▀  " ;;
-            K) line1+=" █ ▄ "; line2+=" █▀  "; line3+=" ▀ ▀ " ;;
-            L) line1+=" █   "; line2+=" █   "; line3+=" ▀▀▀ " ;;
-            M) line1+=" █▄█ "; line2+=" █ █ "; line3+=" ▀ ▀ " ;;
-            N) line1+=" █▄█ "; line2+=" █ █ "; line3+=" ▀ ▀ " ;;
-            O) line1+=" █▀█ "; line2+=" █ █ "; line3+=" ▀▀▀ " ;;
-            P) line1+=" █▀▀ "; line2+=" █▀▀ "; line3+=" ▀   " ;;
-            Q) line1+=" █▀█ "; line2+=" █ █ "; line3+=" ▀▀█ " ;;
-            R) line1+=" █▀▄ "; line2+=" █▀▄ "; line3+=" ▀ ▀ " ;;
-            S) line1+=" █▀▀ "; line2+=" ▀▀█ "; line3+=" ▀▀▀ " ;;
-            T) line1+=" ▀█▀ "; line2+="  █  "; line3+="  ▀  " ;;
-            U) line1+=" █ █ "; line2+=" █ █ "; line3+=" ▀▀▀ " ;;
-            V) line1+=" █ █ "; line2+=" █ █ "; line3+="  ▀  " ;;
-            W) line1+=" █ █ "; line2+=" █▄█ "; line3+=" ▀ ▀ " ;;
-            X) line1+=" █ █ "; line2+="  █  "; line3+=" ▀ ▀ " ;;
-            Y) line1+=" █ █ "; line2+="  █  "; line3+="  ▀  " ;;
-            Z) line1+=" ▀▀█ "; line2+="  █  "; line3+=" █▀▀ " ;;
-            " ") line1+="     "; line2+="     "; line3+="     " ;;
-            1) line1+=" ▄█  "; line2+="  █  "; line3+=" ▄█▄ " ;;
-            2) line1+=" ▀▀█ "; line2+=" █▀▀ "; line3+=" ▀▀▀ " ;;
-            3) line1+=" ▀▀█ "; line2+="  ▀█ "; line3+=" ▀▀▀ " ;;
-            4) line1+=" █ █ "; line2+=" ▀▀█ "; line3+="   █ " ;;
-            5) line1+=" █▀▀ "; line2+=" ▀▀█ "; line3+=" ▀▀▀ " ;;
-            6) line1+=" █▀▀ "; line2+=" █▀█ "; line3+=" ▀▀▀ " ;;
-            7) line1+=" ▀▀█ "; line2+="   █ "; line3+="   ▀ " ;;
-            8) line1+=" █▀█ "; line2+=" █▀█ "; line3+=" ▀▀▀ " ;;
-            9) line1+=" █▀█ "; line2+=" ▀▀█ "; line3+=" ▀▀▀ " ;;
-            0) line1+=" █▀█ "; line2+=" █ █ "; line3+=" ▀▀▀ " ;;
-            "@") line1+=" ▄▀▀ "; line2+=" █ ▀ "; line3+=" ▀▀▀ " ;;
-            "-") line1+="     "; line2+=" ▀▀▀ "; line3+="     " ;;
-            "_") line1+="     "; line2+="     "; line3+=" ▀▀▀ " ;;
-            ".") line1+="     "; line2+="     "; line3+="  ▀  " ;;
-            *) line1+="  ▄  "; line2+=" ▄█▄ "; line3+="  ▀  " ;;
+            A) line1+="   ▄▄▄   "; line2+="  █   █  "; line3+="  █▀▀▀█  "; line4+="  ▀   ▀  " ;;
+            B) line1+="  ██▀▀▄  "; line2+="  ██▀▀▄  "; line3+="  ██▄▄▀  "; line4+="  ▀▀▀▀   " ;;
+            C) line1+="   ▄▀▀▀  "; line2+="  █      "; line3+="  █▄▄▄▀  "; line4+="   ▀▀▀   " ;;
+            D) line1+="  ██▀▀▄  "; line2+="  ██  █  "; line3+="  ██▄▄▀  "; line4+="  ▀▀▀▀   " ;;
+            E) line1+="  █████  "; line2+="  ██▀▀▀  "; line3+="  ██▄▄▄  "; line4+="  ▀▀▀▀▀  " ;;
+            F) line1+="  █████  "; line2+="  ██▀▀▀  "; line3+="  ██     "; line4+="  ▀▀     " ;;
+            G) line1+="   ▄▀▀▀  "; line2+="  █  ▄▄  "; line3+="  █▄▄▄█  "; line4+="   ▀▀▀   " ;;
+            H) line1+="  ██  ██ "; line2+="  ██▀▀██ "; line3+="  ██  ██ "; line4+="  ▀▀  ▀▀ " ;;
+            I) line1+="   ███   "; line2+="    █    "; line3+="    █    "; line4+="   ███   " ;;
+            J) line1+="     ███ "; line2+="      █  "; line3+="  █   █  "; line4+="   ▀▀▀   " ;;
+            K) line1+="  ██  █  "; line2+="  ██▀▀   "; line3+="  ██  █  "; line4+="  ▀▀  ▀  " ;;
+            L) line1+="  ██     "; line2+="  ██     "; line3+="  ██▄▄▄  "; line4+="  ▀▀▀▀▀  " ;;
+            M) line1+="  █ ▄ █  "; line2+="  █▀ ▀█  "; line3+="  █   █  "; line4+="  ▀   ▀  " ;;
+            N) line1+="  █▄  █  "; line2+="  █ █ █  "; line3+="  █  ▀█  "; line4+="  ▀   ▀  " ;;
+            O) line1+="   ▄▀▀▀▄ "; line2+="  █     █"; line3+="   ▀▄▄▄▀ "; line4+="    ▀▀▀  " ;;
+            P) line1+="  ██▀▀▄  "; line2+="  ██▀▀▀  "; line3+="  ██     "; line4+="  ▀▀     " ;;
+            Q) line1+="   ▄▀▀▀▄ "; line2+="  █     █"; line3+="   ▀▄▄▀▀▄"; line4+="    ▀▀▀ ▀" ;;
+            R) line1+="  ██▀▀▄  "; line2+="  ██▀▀▀  "; line3+="  ██  █  "; line4+="  ▀▀  ▀  " ;;
+            S) line1+="   ▄▀▀▀  "; line2+="   ▀▀▀▄  "; line3+="  ▀▀▀▀   "; line4+="   ▀▀▀   " ;;
+            T) line1+="  █████  "; line2+="    █    "; line3+="    █    "; line4+="    ▀    " ;;
+            U) line1+="  ██  ██ "; line2+="  ██  ██ "; line3+="  ▀▀▄▄▀▀ "; line4+="    ▀▀   " ;;
+            V) line1+="  ██  ██ "; line2+="  ▀█  █▀ "; line3+="   ▀▄▄▀  "; line4+="    ▀▀   " ;;
+            W) line1+="  █   █  "; line2+="  █ █ █  "; line3+="  ▀▄▀▄▀  "; line4+="   ▀ ▀   " ;;
+            X) line1+="  ██  ██ "; line2+="   ▀██▀  "; line3+="   ▄██▄  "; line4+="  ██  ██ " ;;
+            Y) line1+="  ██  ██ "; line2+="   ▀██▀  "; line3+="    ██   "; line4+="    ▀▀   " ;;
+            Z) line1+="  █████  "; line2+="    ▄█▀  "; line3+="   █▄▄▄▄ "; line4+="  ▀▀▀▀▀  " ;;
+            1) line1+="   ▄█    "; line2+="    █    "; line3+="    █    "; line4+="   ▄█▄   " ;;
+            2) line1+="   ▄▀▀▀▄ "; line2+="      ▄▀ "; line3+="    ▄▀   "; line4+="   █▄▄▄▄ " ;;
+            3) line1+="   ▀▀▀▀▄ "; line2+="     ▄▀  "; line3+="      ▀▄ "; line4+="   ▀▀▀▀  " ;;
+            4) line1+="   █  █  "; line2+="   █▄▄█▄ "; line3+="      █  "; line4+="      ▀  " ;;
+            5) line1+="   █▀▀▀▀ "; line2+="   ▀▀▀▀▄ "; line3+="      ▄▀ "; line4+="   ▀▀▀▀  " ;;
+            6) line1+="    ▄▀▀▀ "; line2+="   █▀▀▀▄ "; line3+="   ▀▄▄▄▀ "; line4+="    ▀▀▀  " ;;
+            7) line1+="   ▀▀▀▀█ "; line2+="      █  "; line3+="     █   "; line4+="    ▀    " ;;
+            8) line1+="    ▄▀▀▄ "; line2+="    ▄▀▀▄ "; line3+="   ▀▄▄▄▀ "; line4+="    ▀▀▀  " ;;
+            9) line1+="    ▄▀▀▄ "; line2+="    ▀▀▀█ "; line3+="   ▀▄▄▄▀ "; line4+="    ▀▀▀  " ;;
+            0) line1+="    ▄▀▀▄ "; line2+="   █  █  "; line3+="   ▀▄▄▄▀ "; line4+="    ▀▀▀  " ;;
+            "@") line1+="   ▄▀▀▄  "; line2+="  █ █▀▄  "; line3+="  ▀▄▄▄▀  "; line4+="  ▀▀▀▀▀  " ;;
+            "-") line1+="         "; line2+="   ▀▀▀▀  "; line3+="         "; line4+="         " ;;
+            "_") line1+="         "; line2+="         "; line3+="         "; line4+="   ▀▀▀▀  " ;;
+            ".") line1+="         "; line2+="         "; line3+="         "; line4+="    ▀    " ;;
+            " ") line1+="    "; line2+="    "; line3+="    "; line4+="    " ;;
+            *) line1+="   ▄▄▄  "; line2+="  █ ? █ "; line3+="   ▀▄▀  "; line4+="    ▀   " ;;
         esac
     done
 
     _output+=("${line1}")
     _output+=("${line2}")
     _output+=("${line3}")
+    _output+=("${line4}")
 }
--- lib/render.sh
+++ lib/render.sh
@@ -84,14 +84,23 @@ render_title() {
 build_info_lines() {
     local -n _result=$1
 
-    # ASCII Art Username@Hostname
-    local ascii_lines=()
-    render_ascii_text "${INFO_USER}@${INFO_HOSTNAME}" ascii_lines
-    for al in "${ascii_lines[@]}"; do
-        _result+=("$(echo -e "${T_TITLE}${al}${RST}")")
+    # ASCII Art Username and Hostname
+    local ascii_user=()
+    local ascii_host=()
+    render_ascii_text "${INFO_USER}" ascii_user
+    render_ascii_text "${INFO_HOSTNAME}" ascii_host
+    
+    local separator_lines=(
+        "   "
+        " ✦ "
+        " ✦ "
+        "   "
+    )
+
+    for i in 0 1 2 3; do
+        _result+=("$(echo -e "${T_TITLE}${ascii_user[$i]}${RST}${T_DIM}${separator_lines[$i]}${RST}${T_TITLE}${ascii_host[$i]}${RST}")")
     done
     
-    _result+=("$(echo -e "  ${T_DIM}  --------------------------------${RST}")")
     _result+=("")
 
     # ── System Section ────────────
